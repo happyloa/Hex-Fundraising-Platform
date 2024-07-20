e01289c3539bbd4a9169f32cea99c4bb2bb1f8b7
AaronHsieh <piafyoyo06@gmail.com>
2024-07-20 11:25:13 +0800

    完成 Footer

--- components/Layout/Footer.vue
+++ components/Layout/Footer.vue
@@ -1,3 +1,18 @@
 <template>
-  <footer>我是 Footer</footer>
+  <footer class="footer">
+    <p class="text-white text-center m-0">
+      Copyright ©2021 拼拼 All rights reserved.
+    </p>
+  </footer>
 </template>
+
+<style scoped>
+.footer {
+  background-color: #494846;
+  padding: 32px 20px;
+}
+
+p {
+  letter-spacing: 0.2px;
+}
+</style>
