179aaf7742c3bb4caa47bae38f6c154d138a249d
AaronHsieh <piafyoyo06@gmail.com>
2024-07-21 23:44:42 +0800

    將四個 icon 拆分成獨立元件

---
+++ components/Product/Icons.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="d-flex gap-4">
+    <img src="/icon/icon_good01.svg" class="icon" alt="收藏" />
+    <img src="/icon/icon_good02.svg" class="icon" alt="一定是大拇指" />
+    <img src="/icon/icon_good03.svg" class="icon" alt="品質保證" />
+    <img src="/icon/icon_good04.svg" class="icon" alt="品質保證" />
+  </div>
+</template>
+
+<style scoped>
+.icon {
+  cursor: pointer;
+  height: 24px;
+  transition: all 0.3s ease-in-out;
+}
+
+.icon:hover {
+  transform: scale(1.1);
+}
+</style>
