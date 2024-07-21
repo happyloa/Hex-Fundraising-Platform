8ae57e5bc8c56685bd5136cc329793bfe21fb9de
AaronHsieh <piafyoyo06@gmail.com>
2024-07-21 23:44:17 +0800

    將進度條拆分成獨立元件

---
+++ components/Product/ProgressBar.vue
@@ -0,0 +1,27 @@
+<template>
+  <div
+    class="progressBarTrack progress rounded-3"
+    role="progressbar"
+    aria-valuenow="41"
+    aria-valuemin="0"
+    aria-valuemax="100">
+    <div
+      class="progressBarProgress progress-bar fw-semibold"
+      style="width: 41%">
+      41%
+    </div>
+  </div>
+</template>
+
+<style>
+.progressBarTrack {
+  background-color: #eeeeee;
+  margin-bottom: 20px;
+}
+
+.progressBarProgress {
+  background-color: #ffdf65;
+  color: #858377;
+  font-size: 12px;
+}
+</style>
