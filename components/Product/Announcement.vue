bbdf0a6108f38eb62d7bcd5507b634277b1738a1
AaronHsieh <piafyoyo06@gmail.com>
2024-07-22 00:52:48 +0800

    完成募資注意事項文字

--- components/Product/Announcement.vue
+++ components/Product/Announcement.vue
@@ -1,16 +1,26 @@
 <template>
-  <div class="announcementMsg rounded-3" role="alert">
-    <p class="m-0">
+  <div class="announcementContainer rounded-3" role="alert">
+    <p class="announcementMsg m-0">
       專案募資中！<br />
-      在 <span>2021/06/14 23:59</span> 募資結束前，您都可以贊助此計畫。
+      在
+      <time class="announcementTime lh-base fw-bold">2021/06/14 23:59</time>
+      募資結束前，您都可以贊助我們！
     </p>
   </div>
 </template>
 
 <style scoped>
-.announcementMsg {
+.announcementContainer {
   background-color: #ffffff;
   border-left: 3px solid #ffdf65;
   padding: 14px 20px 14px 16px;
+}
+
+.announcementMsg {
+  color: #636057;
+}
+
+.announcementTime {
+  color: #000027;
 }
 </style>
