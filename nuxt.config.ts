791c872eff1df62b2444ed969a375af3b1095210
AaronHsieh <piafyoyo06@gmail.com>
2024-07-22 00:12:08 +0800

    新增 Nuxt UI

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
-  modules: ["usebootstrap", "@nuxtjs/google-fonts"],
+  modules: ["usebootstrap", "@nuxtjs/google-fonts", "@nuxt/ui"],
   googleFonts: {
     families: {
       "Noto+Sans+TC": true,
