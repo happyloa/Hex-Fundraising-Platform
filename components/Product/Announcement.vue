24acebf3cb718fce0231a298d9363586d6f4cfd4
AaronHsieh <piafyoyo06@gmail.com>
2024-07-22 00:36:15 +0800

    將注意事項拆分成獨立元件

---
+++ components/Product/Announcement.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="announcementMsg rounded-3" role="alert">
+    <p class="m-0">
+      專案募資中！<br />
+      在 <span>2021/06/14 23:59</span> 募資結束前，您都可以贊助此計畫。
+    </p>
+  </div>
+</template>
+
+<style scoped>
+.announcementMsg {
+  background-color: #ffffff;
+  border-left: 3px solid #ffdf65;
+  padding: 14px 20px 14px 16px;
+}
+</style>
