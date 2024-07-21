aeb8fa394051cc086c4f7580d6f37e6f1bd769cf
AaronHsieh <piafyoyo06@gmail.com>
2024-07-22 01:06:08 +0800

    新增響應式設定

--- components/Product/Intro.vue
+++ components/Product/Intro.vue
@@ -2,11 +2,11 @@
   <section class="wrapper">
     <div class="container">
       <span
-        class="productCategory d-inline-block text-white fw-bold rounded-1 mb-1"
+        class="productCategory d-inline-block text-white fw-bold rounded-1 mb-2 mb-sm-1"
         >魔法科技</span
       >
-      <h1 class="productName fs-2 fw-bold mb-4">
-        拍出會動的照片｜LivePhotos 魔法拍立得
+      <h1 class="productName fs-2 fw-bold mb-3 mb-sm-4">
+        拍出會動的照片｜<br class="d-sm-none" />LivePhotos 魔法拍立得
       </h1>
       <div class="row">
         <div class="col-md-6">
@@ -38,5 +38,16 @@
 .productName {
   color: #494846;
   letter-spacing: 3px;
+}
+
+@media (max-width: 576px) {
+  .wrapper {
+    padding: 16px 0 32px;
+  }
+
+  .productName {
+    color: #53524f;
+    letter-spacing: 2.25px;
+  }
 }
 </style>
