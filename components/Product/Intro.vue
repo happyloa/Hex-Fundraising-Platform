f07b958f49fa72f645723e678b1e9ac3f0440960
AaronHsieh <piafyoyo06@gmail.com>
2024-07-21 12:50:22 +0800

    新增產品相關資訊元件資料夾

---
+++ components/Product/Intro.vue
@@ -0,0 +1,33 @@
+<template>
+  <section class="wrapper">
+    <div class="container">
+      <span class="productCategory text-white fw-bold rounded-1 mb-1"
+        >魔法科技</span
+      >
+      <h1 class="heading fs-2 fw-bold mb-4">
+        拍出會動的照片｜LivePhotos 魔法拍立得
+      </h1>
+      <div class="row">
+        <div class="col-12 col-md-6">1</div>
+        <div class="col-12 col-md-6">2</div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+.wrapper {
+  background-color: #f8f9fa;
+  padding: 48px 0 64px;
+}
+
+.productCategory {
+  background: #858377;
+  padding: 1px 16px 3px;
+}
+
+.heading {
+  color: #494846;
+  letter-spacing: 3px;
+}
+</style>
