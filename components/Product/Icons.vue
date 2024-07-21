b8467cdb74252a170bc8a2408894c6ebef1b4c70
AaronHsieh <piafyoyo06@gmail.com>
2024-07-22 00:54:39 +0800

    新增 UTooltip 元件

--- components/Product/Icons.vue
+++ components/Product/Icons.vue
@@ -1,9 +1,23 @@
 <template>
   <div class="d-flex gap-4 mb-3">
-    <img src="/icon/icon_good01.svg" class="icon" alt="收藏" />
-    <img src="/icon/icon_good02.svg" class="icon" alt="一定是大拇指" />
-    <img src="/icon/icon_good03.svg" class="icon" alt="品質保證" />
-    <img src="/icon/icon_good04.svg" class="icon" alt="品質保證" />
+    <UTooltip text="收藏" :popper="{ placement: 'top', gpuAcceleration: true }">
+      <img src="/icon/icon_good01.svg" class="icon" alt="收藏" />
+    </UTooltip>
+    <UTooltip
+      text="一定是大拇指"
+      :popper="{ placement: 'top', gpuAcceleration: true }">
+      <img src="/icon/icon_good02.svg" class="icon" alt="一定是大拇指" />
+    </UTooltip>
+    <UTooltip
+      text="品質保證"
+      :popper="{ placement: 'top', gpuAcceleration: true }">
+      <img src="/icon/icon_good03.svg" class="icon" alt="品質保證" />
+    </UTooltip>
+    <UTooltip
+      text="品質保證"
+      :popper="{ placement: 'top', gpuAcceleration: true }">
+      <img src="/icon/icon_good04.svg" class="icon" alt="品質保證" />
+    </UTooltip>
   </div>
 </template>
 
