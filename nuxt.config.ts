4280d9953012c0d70363258e2b5bbf021326a70c
AaronHsieh <piafyoyo06@gmail.com>
2024-07-22 10:55:21 +0800

    新增網站標題

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,15 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
+  // 網站標題設定
+  app: {
+    head: {
+      title: "拚拚 - 募資平臺切版練習",
+    },
+  },
+  // 使用到的套件
   modules: ["usebootstrap", "@nuxtjs/google-fonts", "@nuxt/ui"],
+  // 網站上使用到的字型（來自 @nuxtjs/google-fonts）
   googleFonts: {
     families: {
       "Noto+Sans+TC": true,
