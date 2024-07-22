e7041a0e6d3928bffb162b0e41190d3fa83aaab3
AaronHsieh <piafyoyo06@gmail.com>
2024-07-22 23:35:51 +0800

    當螢幕寬度大於或等於 768px 時移除 margin

--- components/Product/Icons.vue
+++ components/Product/Icons.vue
@@ -2,32 +2,41 @@
   <div class="d-flex gap-4 mb-3">
     <UTooltip text="收藏" :popper="{ placement: 'top', gpuAcceleration: true }">
       <div class="d-flex align-items-center">
-        <img src="/icon/icon_good01.svg" class="icon mr-2" alt="收藏" />
-        <span class="iconText d-md-none">認證標章</span>
+        <img src="/icon/icon_good01.svg" class="icon mr-2 m-md-0" alt="收藏" />
+        <span class="iconText d-md-none text-nowrap">認證標章</span>
       </div>
     </UTooltip>
     <UTooltip
       text="一定是大拇指"
       :popper="{ placement: 'top', gpuAcceleration: true }">
       <div class="d-flex align-items-center">
-        <img src="/icon/icon_good02.svg" class="icon mr-2" alt="一定是大拇指" />
-        <span class="iconText d-md-none">品質保證</span>
+        <img
+          src="/icon/icon_good02.svg"
+          class="icon mr-2 m-md-0"
+          alt="一定是大拇指" />
+        <span class="iconText d-md-none text-nowrap">品質保證</span>
       </div>
     </UTooltip>
     <UTooltip
       text="品質保證"
       :popper="{ placement: 'top', gpuAcceleration: true }">
       <div class="d-flex align-items-center">
-        <img src="/icon/icon_good03.svg" class="icon mr-2" alt="品質保證" />
-        <span class="iconText d-md-none">原生創意</span>
+        <img
+          src="/icon/icon_good03.svg"
+          class="icon mr-2 m-md-0"
+          alt="品質保證" />
+        <span class="iconText d-md-none text-nowrap">原生創意</span>
       </div>
     </UTooltip>
     <UTooltip
       text="品質保證"
       :popper="{ placement: 'top', gpuAcceleration: true }">
       <div class="d-flex align-items-center">
-        <img src="/icon/icon_good04.svg" class="icon mr-2" alt="品質保證" />
-        <span class="iconText d-md-none">人氣王</span>
+        <img
+          src="/icon/icon_good04.svg"
+          class="icon mr-2 m-md-0"
+          alt="品質保證" />
+        <span class="iconText d-md-none text-nowrap">人氣王</span>
       </div>
     </UTooltip>
   </div>
