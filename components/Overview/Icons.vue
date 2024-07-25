0592260dc9309fef248004c2899f2da83b533f93
AaronHsieh <piafyoyo06@gmail.com>
2024-07-26 03:13:08 +0800

    移除 Nuxt UI 元件

--- components/Overview/Icons.vue
+++ components/Overview/Icons.vue
@@ -1,44 +1,30 @@
 <template>
   <div class="d-flex flex-wrap gap-x-4 gap-y-2 mb-3">
-    <UTooltip text="收藏" :popper="{ placement: 'top', gpuAcceleration: true }">
-      <div class="d-flex align-items-center">
-        <img src="/icon/icon_good01.svg" class="icon mr-2 m-md-0" alt="收藏" />
-        <span class="iconText d-md-none text-nowrap">認證標章</span>
-      </div>
-    </UTooltip>
-    <UTooltip
-      text="一定是大拇指"
-      :popper="{ placement: 'top', gpuAcceleration: true }">
-      <div class="d-flex align-items-center">
-        <img
-          src="/icon/icon_good02.svg"
-          class="icon mr-2 m-md-0"
-          alt="一定是大拇指" />
-        <span class="iconText d-md-none text-nowrap">品質保證</span>
-      </div>
-    </UTooltip>
-    <UTooltip
-      text="品質保證"
-      :popper="{ placement: 'top', gpuAcceleration: true }">
-      <div class="d-flex align-items-center">
-        <img
-          src="/icon/icon_good03.svg"
-          class="icon mr-2 m-md-0"
-          alt="品質保證" />
-        <span class="iconText d-md-none text-nowrap">原生創意</span>
-      </div>
-    </UTooltip>
-    <UTooltip
-      text="品質保證"
-      :popper="{ placement: 'top', gpuAcceleration: true }">
-      <div class="d-flex align-items-center">
-        <img
-          src="/icon/icon_good04.svg"
-          class="icon mr-2 m-md-0"
-          alt="品質保證" />
-        <span class="iconText d-md-none text-nowrap">人氣王</span>
-      </div>
-    </UTooltip>
+    <div class="d-flex align-items-center">
+      <img src="/icon/icon_good01.svg" class="icon mr-2 m-md-0" alt="收藏" />
+      <span class="iconText d-md-none text-nowrap">認證標章</span>
+    </div>
+    <div class="d-flex align-items-center">
+      <img
+        src="/icon/icon_good02.svg"
+        class="icon mr-2 m-md-0"
+        alt="一定是大拇指" />
+      <span class="iconText d-md-none text-nowrap">品質保證</span>
+    </div>
+    <div class="d-flex align-items-center">
+      <img
+        src="/icon/icon_good03.svg"
+        class="icon mr-2 m-md-0"
+        alt="品質保證" />
+      <span class="iconText d-md-none text-nowrap">原生創意</span>
+    </div>
+    <div class="d-flex align-items-center">
+      <img
+        src="/icon/icon_good04.svg"
+        class="icon mr-2 m-md-0"
+        alt="品質保證" />
+      <span class="iconText d-md-none text-nowrap">人氣王</span>
+    </div>
   </div>
 </template>
 
